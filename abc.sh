33ca9846a0168091afafae68cb23b67c2d18558f
arjun.panwar <arjun.panwar@tothenew.com>
2022-04-22 09:20:34 +0530

    sh abc.sh

---
+++ abc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo Hello World >> /home/arjun/Downloads/abc.log
+echo "Hello"
+echo
+echo "User:" `whoami`
+echo
+echo "Directory" $( pwd )
+echo
+echo "OS"
+lsb_release -d
+echo "Addition of two no."
+NUM = $(($numone + $numtwo))
+echo "$NUM"
